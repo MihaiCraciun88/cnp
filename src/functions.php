81d5550a2287e394e1695a254e4c427177e3c77d
MihaiCraciun88 <31093928+MihaiCraciun88@users.noreply.github.com>
2022-03-25 21:47:16 +0200

    initial commit

---
+++ src/functions.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ *  S AA LL ZZ JJ NNN C
+ */
+function isCnpValid(string $value) : bool {
+    if (strlen($value) !== 13 || preg_match('/[^0-9]/', $value)) {
+        return false;
+    }
+
+    $date = getCnpDate($value);
+    if (checkdate($date['month'], $date['day'], $date['year']) === false) {
+        return false;
+    }
+
+    switch ((int) $value[0]) {
+        case 1:
+        case 2:
+            if (!isBetween($date['year'], 1900, 1999)) {
+                return false;
+            }
+            break;
+        case 5:
+        case 6:
+            if (!isBetween($date['year'], 2000, 2099)) {
+                return false;
+            }
+            break;
+    }
+
+    if (getCountyName($value[7] . $value[8]) === null) {
+        return false;
+    }
+
+    if (cnpCheckSum($value) === false) {
+        return false;
+    }
+    
+    return true;
+}
+
+function getCnpDate(string $value) : array {
+    $dt = DateTime::createFromFormat('y', $value[1] . $value[2]);
+    return [
+        'day'   => (int) ($value[5] . $value[6]),
+        'month' => (int) ($value[3] . $value[4]),
+        'year'  => (int) $dt->format('Y'),
+    ];
+}
+
+function isBetween(int $value, int $min, int $max) : bool {
+    return $value >= $min && $value <= $max;
+}
+
+function getCountyName(string $code) : ?string {
+    $names = [
+        '01' => 'Alba',
+        '02' => 'Arad',
+        '03' => 'Argeș',
+        '04' => 'Bacău',
+        '05' => 'Bihor',
+        '06' => 'Bistrița-Năsăud',
+        '07' => 'Botoșani',
+        '08' => 'Brașov',
+        '09' => 'Brăila',
+        '10' => 'Buzău',
+        '11' => 'Caraș-Severin',
+        '12' => 'Cluj',
+        '13' => 'Constanța',
+        '14' => 'Covasna',
+        '15' => 'Dâmbovița',
+        '16' => 'Dolj',
+        '17' => 'Galați',
+        '18' => 'Gorj',
+        '19' => 'Harghita',
+        '20' => 'Hunedoara',
+        '21' => 'Ialomița',
+        '22' => 'Iași',
+        '23' => 'Ilfov',
+        '24' => 'Maramureș',
+        '25' => 'Mehedinți',
+        '26' => 'Mureș',
+        '27' => 'Neamț',
+        '28' => 'Olt',
+        '29' => 'Prahova',
+        '30' => 'Satu Mare',
+        '31' => 'Sălaj',
+        '32' => 'Sibiu',
+        '33' => 'Suceava',
+        '34' => 'Teleorman',
+        '35' => 'Timiș',
+        '36' => 'Tulcea',
+        '37' => 'Vaslui',
+        '38' => 'Vâlcea',
+        '39' => 'Vrancea',
+        '40' => 'București',
+        '41' => 'București - Sector 1',
+        '42' => 'București - Sector 2',
+        '43' => 'București - Sector 3',
+        '44' => 'București - Sector 4',
+        '45' => 'București - Sector 5',
+        '46' => 'București - Sector 6',
+        '51' => 'Călărași',
+        '52' => 'Giurgiu',
+    ];
+    return $names[$code] ?? null;
+}
+
+function cnpCheckSum(string $value) : bool {
+    $ctrlString = '279146358279';
+    $sum = 0;
+    for ($i = 0; $i <= 11; $i++) {
+        $sum += intval($ctrlString[$i]) * intval($value[$i]);
+    }
+    return (($sum % 11) % 10) === intval($value[12]);
+}
---
+++ tests/CnpTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class CnpTest extends TestCase
+{
+    protected $cnp = '1880118430085';
+
+    public function testInvalidStringLen() {
+        $this->assertFalse(isCnpValid('123'));
+    }
+
+    public function testIsNotNumeric()
+    {
+        $this->assertFalse(isCnpValid('1234567890ABC'));
+    }
+
+    public function testInvalidDate() {
+        $cnp = $this->cnp;
+
+        // 1999-02-29
+        $cnp[1] = '9';
+        $cnp[2] = '9';
+        $cnp[3] = '0';
+        $cnp[4] = '2';
+        $cnp[5] = '2';
+        $cnp[6] = '9';
+        $this->assertFalse(isCnpValid($cnp));
+
+        // 5 1999-02-28
+        $cnp[0] = '5';
+        $cnp[6] = '8';
+        $this->assertFalse(isCnpValid($cnp));
+
+        // 1 2002-02-28
+        $cnp[0] = '1';
+        $cnp[1] = '0';
+        $cnp[2] = '2';
+        $this->assertFalse(isCnpValid($cnp));
+    }
+
+    public function testCheckSum() {
+        $this->assertTrue(cnpCheckSum($this->cnp));
+    }
+
+    public function testValid() {
+        $this->assertTrue(isCnpValid($this->cnp));
+    }
+}
