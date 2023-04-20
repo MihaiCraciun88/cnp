aa890e44402238920bd15d7d263389268959304c
MihaiCraciun88 <31093928+MihaiCraciun88@users.noreply.github.com>
2023-04-19 22:06:01 -0700

    C style

--- cnp.php
+++ cnp.php
@@ -2,15 +2,21 @@
 // include 'vendor/autoload.php';
 include 'src/functions.php';
 
-if ($argc < 2) {
-    echo 'Introdu CNP';
-    exit(-1);
+function main(int $argc, array $argv): int
+{
+    if ($argc < 2) {
+        echo 'Introdu CNP';
+        return -1;
+    }
+
+    $cnp = $argv[1];
+    if (!isCnpValid($cnp)) {
+        echo 'CNP invalid';
+        return -2;
+    }
+
+    echo 'CNP valid';
+    return 0;
 }
 
-$cnp = $argv[1];
-if (!isCnpValid($cnp)) {
-    echo 'CNP invalid';
-    exit(-2);
-}
-
-echo 'CNP valid';
+exit(main($argc, $argv));
